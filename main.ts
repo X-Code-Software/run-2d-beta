0875a30d7877a98ccbead01ea650f2c0c1b1a2e3
X-Code-Software <69800526+X-Code-Software@users.noreply.github.com>
2020-08-18 14:10:53 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -43,42 +43,6 @@ game.onUpdateInterval(100, function () {
             . f f f f f f f f f f . . 
             . . f f . . . f f f . . . 
             `)
-        Runner_blue.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . f f f f f f . . . . 
-            . f f f f f f f f f . . . 
-            . f f f f f f c f f f . . 
-            f f f f c f f f c f f f . 
-            f c f f c c f f f c c f f 
-            f c c f f f f e f f f f f 
-            f f f f f f f e e f f f . 
-            f f e e f b f e e f f . . 
-            . f e 4 e 1 f 4 4 f f . . 
-            . f f f e e 4 4 4 f . . . 
-            . . f e 4 4 e e f f . . . 
-            . . f e 4 4 e 7 7 f . . . 
-            . f f f e e f 6 6 f f . . 
-            . f f f f f f f f f f . . 
-            . . f f . . . f f f . . . 
-            `)
-        Runner_blue.setImage(img`
-            . . . f f f f f . . . . . 
-            . f f f f f f f f f . . . 
-            . f f f f f f c f f f . . 
-            f f f f c f f f c f f . . 
-            f c f f c c f f f c c f f 
-            f c c f f f f e f f f f f 
-            f f f f f f f e e f f f . 
-            f f e e f b f e e f f . . 
-            . f e 4 e 1 f 4 4 f . . . 
-            . f f f e 4 4 4 4 f . . . 
-            . . f e e e e e f f . . . 
-            . . e 4 4 e 7 7 7 f . . . 
-            . . e 4 4 e 7 7 7 f . . . 
-            . . f e e f 6 6 6 f . . . 
-            . . . f f f f f f . . . . 
-            . . . . f f f . . . . . . 
-            `)
     }
     if (controller.left.isPressed()) {
         Runner_blue.setImage(img`
@@ -98,42 +62,6 @@ game.onUpdateInterval(100, function () {
             . . f f 6 6 6 6 f e e f . 
             . . f f f f f f f f f f . 
             . . . f f f . . . f f . . 
-            `)
-        Runner_blue.setImage(img`
-            . . . . . . . . . . . . . 
-            . . . . f f f f f f . . . 
-            . . . f f f f f f f f f . 
-            . . f f f c f f f f f f . 
-            . f f f c f f f c f f f f 
-            f f c c f f f c c f f c f 
-            f f f f f e f f f f c c f 
-            . f f f e e f f f f f f f 
-            . . f f e e f b f e e f f 
-            . . f f 4 4 f 1 e 4 e f . 
-            . . . f 4 4 4 e e f f f . 
-            . . . f f e e 4 4 e f . . 
-            . . . f 7 7 e 4 4 e f . . 
-            . . f f 6 6 f e e f f f . 
-            . . f f f f f f f f f f . 
-            . . . f f f . . . f f . . 
-            `)
-        Runner_blue.setImage(img`
-            . . . . . f f f f f . . . 
-            . . . f f f f f f f f f . 
-            . . f f f c f f f f f f . 
-            . . f f c f f f c f f f f 
-            f f c c f f f c c f f c f 
-            f f f f f e f f f f c c f 
-            . f f f e e f f f f f f f 
-            . . f f e e f b f e e f f 
-            . . . f 4 4 f 1 e 4 e f . 
-            . . . f 4 4 4 4 e f f f . 
-            . . . f f e e e e e f . . 
-            . . . f 7 7 7 e 4 4 e . . 
-            . . . f 7 7 7 e 4 4 e . . 
-            . . . f 6 6 6 f e e f . . 
-            . . . . f f f f f f . . . 
-            . . . . . . f f f . . . . 
             `)
     }
 })
