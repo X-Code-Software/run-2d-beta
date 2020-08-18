b1719e814e5e291a4553c2579459e4458ba1d2ef
X-Code-Software <69800526+X-Code-Software@users.noreply.github.com>
2020-08-18 12:25:05 +0100

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,33 @@
+ 
+
+
+> Open this page at [https://x-code-software.github.io/run-2d-beta/](https://x-code-software.github.io/run-2d-beta/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/x-code-software/run-2d-beta** and import
+
+## Edit this project ![Build status badge](https://github.com/x-code-software/run-2d-beta/workflows/MakeCode/badge.svg)
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/x-code-software/run-2d-beta** and click import
+
+## Blocks preview
+
+This image shows the blocks code from the last commit in master.
+This image may take a few minutes to refresh.
+
+![A rendered view of the blocks](https://github.com/x-code-software/run-2d-beta/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,139 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
+let Runner_blue = sprites.create(img`
+    . . . f f f f f . . . . . 
+    . f f f f f f f f f . . . 
+    . f f f f f f c f f f . . 
+    f f f f c f f f c f f . . 
+    f c f f c c f f f c c f f 
+    f c c f f f f e f f f f f 
+    f f f f f f f e e f f f . 
+    f f e e f b f e e f f . . 
+    . f e 4 e 1 f 4 4 f . . . 
+    . f f f e 4 4 4 4 f . . . 
+    . . f e e e e e f f . . . 
+    . . e 4 4 e 7 7 7 f . . . 
+    . . e 4 4 e 7 7 7 f . . . 
+    . . f e e f 6 6 6 f . . . 
+    . . . f f f f f f . . . . 
+    . . . . f f f . . . . . . 
+    `, SpriteKind.Player)
+Runner_blue.setFlag(SpriteFlag.StayInScreen, true)
+controller.moveSprite(Runner_blue, 100, 100)
+game.onUpdateInterval(100, function () {
+    if (controller.right.isPressed()) {
+        Runner_blue.setImage(img`
+            . . . . . . . . . . . . . 
+            . . . f f f f f f . . . . 
+            . f f f f f f f f f . . . 
+            . f f f f f f c f f f . . 
+            f f f f c f f f c f f f . 
+            f c f f c c f f f c c f f 
+            f c c f f f f e f f f f f 
+            f f f f f f f e e f f f . 
+            f f e e f b f e e f f f . 
+            f f e 4 e 1 f 4 4 f f . . 
+            . f f f e 4 4 4 4 f . . . 
+            . 4 4 4 e e e e f f . . . 
+            . e 4 4 e 7 7 7 7 f . . . 
+            . f e e f 6 6 6 6 f f . . 
+            . f f f f f f f f f f . . 
+            . . f f . . . f f f . . . 
+            `)
+        Runner_blue.setImage(img`
+            . . . . . . . . . . . . . 
+            . . . f f f f f f . . . . 
+            . f f f f f f f f f . . . 
+            . f f f f f f c f f f . . 
+            f f f f c f f f c f f f . 
+            f c f f c c f f f c c f f 
+            f c c f f f f e f f f f f 
+            f f f f f f f e e f f f . 
+            f f e e f b f e e f f . . 
+            . f e 4 e 1 f 4 4 f f . . 
+            . f f f e e 4 4 4 f . . . 
+            . . f e 4 4 e e f f . . . 
+            . . f e 4 4 e 7 7 f . . . 
+            . f f f e e f 6 6 f f . . 
+            . f f f f f f f f f f . . 
+            . . f f . . . f f f . . . 
+            `)
+        Runner_blue.setImage(img`
+            . . . f f f f f . . . . . 
+            . f f f f f f f f f . . . 
+            . f f f f f f c f f f . . 
+            f f f f c f f f c f f . . 
+            f c f f c c f f f c c f f 
+            f c c f f f f e f f f f f 
+            f f f f f f f e e f f f . 
+            f f e e f b f e e f f . . 
+            . f e 4 e 1 f 4 4 f . . . 
+            . f f f e 4 4 4 4 f . . . 
+            . . f e e e e e f f . . . 
+            . . e 4 4 e 7 7 7 f . . . 
+            . . e 4 4 e 7 7 7 f . . . 
+            . . f e e f 6 6 6 f . . . 
+            . . . f f f f f f . . . . 
+            . . . . f f f . . . . . . 
+            `)
+    }
+    if (controller.left.isPressed()) {
+        Runner_blue.setImage(img`
+            . . . . . . . . . . . . . 
+            . . . . f f f f f f . . . 
+            . . . f f f f f f f f f . 
+            . . f f f c f f f f f f . 
+            . f f f c f f f c f f f f 
+            f f c c f f f c c f f c f 
+            f f f f f e f f f f c c f 
+            . f f f e e f f f f f f f 
+            . f f f e e f b f e e f f 
+            . . f f 4 4 f 1 e 4 e f f 
+            . . . f 4 4 4 4 e f f f . 
+            . . . f f e e e e 4 4 4 . 
+            . . . f 7 7 7 7 e 4 4 e . 
+            . . f f 6 6 6 6 f e e f . 
+            . . f f f f f f f f f f . 
+            . . . f f f . . . f f . . 
+            `)
+        Runner_blue.setImage(img`
+            . . . . . . . . . . . . . 
+            . . . . f f f f f f . . . 
+            . . . f f f f f f f f f . 
+            . . f f f c f f f f f f . 
+            . f f f c f f f c f f f f 
+            f f c c f f f c c f f c f 
+            f f f f f e f f f f c c f 
+            . f f f e e f f f f f f f 
+            . . f f e e f b f e e f f 
+            . . f f 4 4 f 1 e 4 e f . 
+            . . . f 4 4 4 e e f f f . 
+            . . . f f e e 4 4 e f . . 
+            . . . f 7 7 e 4 4 e f . . 
+            . . f f 6 6 f e e f f f . 
+            . . f f f f f f f f f f . 
+            . . . f f f . . . f f . . 
+            `)
+        Runner_blue.setImage(img`
+            . . . . . f f f f f . . . 
+            . . . f f f f f f f f f . 
+            . . f f f c f f f f f f . 
+            . . f f c f f f c f f f f 
+            f f c c f f f c c f f c f 
+            f f f f f e f f f f c c f 
+            . f f f e e f f f f f f f 
+            . . f f e e f b f e e f f 
+            . . . f 4 4 f 1 e 4 e f . 
+            . . . f 4 4 4 4 e f f f . 
+            . . . f f e e e e e f . . 
+            . . . f 7 7 7 e 4 4 e . . 
+            . . . f 7 7 7 e 4 4 e . . 
+            . . . f 6 6 6 f e e f . . 
+            . . . . f f f f f f . . . 
+            . . . . . . f f f . . . . 
+            `)
+    }
+})
