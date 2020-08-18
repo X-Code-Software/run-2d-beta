14e1e12703a345084e02112303956ec4a3cfb8a5
X-Code-Software <69800526+X-Code-Software@users.noreply.github.com>
2020-08-18 18:50:24 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -33,7 +33,7 @@ let Runner_blue = sprites.create(img`
     . . . . f f f . . . . . . 
     `, SpriteKind.Player)
 Runner_blue.setFlag(SpriteFlag.StayInScreen, true)
-controller.moveSprite(Runner_blue, 100, 100)
+controller.moveSprite(Runner_blue, 75, 0)
 game.onUpdateInterval(100, function () {
     if (controller.right.isPressed()) {
         Runner_blue.setImage(img`
@@ -73,6 +73,31 @@ game.onUpdateInterval(100, function () {
             . . f f 6 6 6 6 f e e f . 
             . . f f f f f f f f f f . 
             . . . f f f . . . f f . . 
+            `)
+    }
+    if (controller.A.isPressed()) {
+        Runner_blue.setVelocity(0, -150)
+    } else {
+        Runner_blue.setVelocity(0, 150)
+    }
+    if (!(controller.left.isPressed() || controller.right.isPressed())) {
+        Runner_blue.setImage(img`
+            . . . f f f f f . . . . . 
+            . f f f f f f f f f . . . 
+            . f f f f f f c f f f . . 
+            f f f f c f f f c f f . . 
+            f c f f c c f f f c c f f 
+            f c c f f f f e f f f f f 
+            f f f f f f f e e f f f . 
+            f f e e f b f e e f f . . 
+            . f e 4 e 1 f 4 4 f . . . 
+            . f f f e 4 4 4 4 f . . . 
+            . . f e e e e e f f . . . 
+            . . e 4 4 e 7 7 7 f . . . 
+            . . e 4 4 e 7 7 7 f . . . 
+            . . f e e f 6 6 6 f . . . 
+            . . . f f f f f f . . . . 
+            . . . . f f f . . . . . . 
             `)
     }
 })
