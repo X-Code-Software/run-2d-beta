1ebc964256d4a538d7218c33c00b993bd8cfb46e
X-Code-Software <69800526+X-Code-Software@users.noreply.github.com>
2020-08-19 09:58:41 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -62,11 +62,17 @@ game.onUpdateInterval(500, function () {
         Skin = 1
         console.log("Skin changed")
     }
+    if (controller.up.isPressed()) {
+        Skin += 1
+    }
+    if (controller.down.isPressed()) {
+        Skin += -1
+    }
 })
 game.onUpdateInterval(100, function () {
     console.log("Animation cycle")
     if (controller.right.isPressed()) {
-        if (true) {
+        if (Skin == 0) {
             Runner_blue.setImage(img`
                 . . . . . . . . . . . . . 
                 . . . f f f f f f . . . . 
@@ -87,27 +93,27 @@ game.onUpdateInterval(100, function () {
                 `)
         } else {
             Runner_blue.setImage(img`
-                . . . . . . . . . . . . . 
-                . . . f f f f f f . . . . 
-                . f f f f f f f f f . . . 
-                . f f f f f f c f f f . . 
-                f f f f c f f f c f f f . 
-                f c f f c c f f f c c f f 
-                f c c f f f f e f f f f f 
-                f f f f f f f e e f f f . 
-                f f e e f b f e e f f f . 
-                f f e 4 e 1 f 4 4 f f . . 
-                . f f f e 4 4 4 4 f . . . 
-                . 4 4 4 e e e e f f . . . 
-                . e 4 4 e 7 7 7 7 f . . . 
-                . f e e f 6 6 6 6 f f . . 
-                . f f f f f f f f f f . . 
-                . . f f . . . f f f . . . 
+                . . . . . . f f f f 4 4 f . . . 
+                . . . . f f b f 5 4 5 5 4 f . . 
+                . . . f b 3 3 e 4 5 5 5 5 f . . 
+                . . f b 3 3 3 3 e 4 4 4 e f . . 
+                . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+                . . f 3 3 3 3 e b 3 e e 3 3 f . 
+                . . f 3 3 3 3 f f e e e 3 3 f . 
+                . . f b b b b f b f e e e 3 f . 
+                . . f b b b b e 1 f 4 4 e f . . 
+                . f f b b b b f 4 4 4 4 f . . . 
+                . f b b b b f f f e e e f . . . 
+                . . f b b f 4 4 e d d d f . . . 
+                . . . f f e 4 4 e d d d f . . . 
+                . . . . f b e e b d b d b f . . 
+                . . . . f f d 1 d 1 d 1 f f . . 
+                . . . . . . f f b b f f . . . . 
                 `)
         }
     }
     if (controller.left.isPressed()) {
-        if (true) {
+        if (Skin == 0) {
             Runner_blue.setImage(img`
                 . . . . . . . . . . . . . 
                 . . . . f f f f f f . . . 
@@ -128,22 +134,22 @@ game.onUpdateInterval(100, function () {
                 `)
         } else {
             Runner_blue.setImage(img`
-                . . . . . . . . . . . . . 
-                . . . . f f f f f f . . . 
-                . . . f f f f f f f f f . 
-                . . f f f c f f f f f f . 
-                . f f f c f f f c f f f f 
-                f f c c f f f c c f f c f 
-                f f f f f e f f f f c c f 
-                . f f f e e f f f f f f f 
-                . f f f e e f b f e e f f 
-                . . f f 4 4 f 1 e 4 e f f 
-                . . . f 4 4 4 4 e f f f . 
-                . . . f f e e e e 4 4 4 . 
-                . . . f 7 7 7 7 e 4 4 e . 
-                . . f f 6 6 6 6 f e e f . 
-                . . f f f f f f f f f f . 
-                . . . f f f . . . f f . . 
+                . . . f 4 4 f f f f . . . . . . 
+                . . f 4 5 5 4 5 f b f f . . . . 
+                . . f 5 5 5 5 4 e 3 3 b f . . . 
+                . . f e 4 4 4 e 3 3 3 3 b f . . 
+                . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+                . f 3 3 e e 3 b e 3 3 3 3 f . . 
+                . f 3 3 e e e f f 3 3 3 3 f . . 
+                . f 3 e e e f b f b b b b f . . 
+                . . f e 4 4 f 1 e b b b b f . . 
+                . . . f 4 4 4 4 f b b b b f f . 
+                . . . f e e e f f f b b b b f . 
+                . . . f d d d e 4 4 f b b f . . 
+                . . . f d d d e 4 4 e f f . . . 
+                . . f b d b d b e e b f . . . . 
+                . . f f 1 d 1 d 1 d f f . . . . 
+                . . . . f f b b f f . . . . . . 
                 `)
         }
     }
@@ -153,7 +159,7 @@ game.onUpdateInterval(100, function () {
         Runner_blue.setVelocity(0, 100)
     }
     if (!(controller.left.isPressed() || controller.right.isPressed())) {
-        if (true) {
+        if (Skin == 0) {
             Runner_blue.setImage(img`
                 . . . f f f f f . . . . . 
                 . f f f f f f f f f . . . 
@@ -174,22 +180,22 @@ game.onUpdateInterval(100, function () {
                 `)
         } else {
             Runner_blue.setImage(img`
-                . . . f f f f f . . . . . 
-                . f f f f f f f f f . . . 
-                . f f f f f f c f f f . . 
-                f f f f c f f f c f f . . 
-                f c f f c c f f f c c f f 
-                f c c f f f f e f f f f f 
-                f f f f f f f e e f f f . 
-                f f e e f b f e e f f . . 
-                . f e 4 e 1 f 4 4 f . . . 
-                . f f f e 4 4 4 4 f . . . 
-                . . f e e e e e f f . . . 
-                . . e 4 4 e 7 7 7 f . . . 
-                . . e 4 4 e 7 7 7 f . . . 
-                . . f e e f 6 6 6 f . . . 
-                . . . f f f f f f . . . . 
-                . . . . f f f . . . . . . 
+                . . . . . f f 4 4 f f . . . . . 
+                . . . . f 5 4 5 5 4 5 f . . . . 
+                . . . f e 4 5 5 5 5 4 e f . . . 
+                . . f b 3 e 4 4 4 4 e 3 b f . . 
+                . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+                . f 3 3 e b 3 e e 3 b e 3 3 f . 
+                . f 3 3 f f e e e e f f 3 3 f . 
+                . f b b f b f e e f b f b b f . 
+                . f b b e 1 f 4 4 f 1 e b b f . 
+                f f b b f 4 4 4 4 4 4 f b b f f 
+                f b b f f f e e e e f f f b b f 
+                . f e e f b d d d d b f e e f . 
+                . . e 4 c d d d d d d c 4 e . . 
+                . . e f b d b d b d b b f e . . 
+                . . . f f 1 d 1 d 1 d f f . . . 
+                . . . . . f f b b f f . . . . . 
                 `)
         }
     }
