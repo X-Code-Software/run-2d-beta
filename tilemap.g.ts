dc164c2ceddc8bd4bb3e758c9d99167a50c1371d
X-Code-Software <69800526+X-Code-Software@users.noreply.github.com>
2020-08-19 14:35:00 +0100

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -34,6 +34,10 @@ namespace myTiles {
     export const tile15 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
 
 }
 // Auto-generated code. Do not edit.
