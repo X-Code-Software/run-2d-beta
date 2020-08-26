c7f9576b73617ace94bbc94df25b912dce253a09
X-Code-Software <69800526+X-Code-Software@users.noreply.github.com>
2020-08-26 12:07:07 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -284,6 +284,26 @@ game.onUpdateInterval(100, function () {
                 . . . . . . . . f . . . . . . . 
                 `)
         }
+        if (Skin == 3) {
+            Runner_blue.setImage(img`
+                . . 2 2 2 2 2 . . . . . 
+                . 2 2 2 2 2 2 2 2 2 . . 
+                . . e e e d f d . . . . 
+                . e d e d d f d d d . . 
+                . e d e e d d d f d d . 
+                . e e d d d f f f f . . 
+                . . . d d d d d d . . . 
+                . . 2 2 8 2 2 2 . . . . 
+                . 2 2 2 8 2 2 8 2 2 2 . 
+                2 2 2 2 8 8 8 8 2 2 2 2 
+                d d 2 8 5 8 8 5 8 2 d d 
+                d d d 8 8 8 8 8 8 d d d 
+                d d 8 8 8 8 8 8 8 8 d d 
+                . . 8 8 8 . . 8 8 8 . . 
+                . e e e . . . . e e e . 
+                e e e e . . . . e e e e 
+                `)
+        }
     }
     if (controller.left.isPressed()) {
         if (Skin == 0) {
@@ -344,6 +364,26 @@ game.onUpdateInterval(100, function () {
                 . . . . . . . . f . . . . . . . 
                 . . . . . . . . f . . . . . . . 
                 . . . . . . . . f . . . . . . . 
+                `)
+        }
+        if (Skin == 3) {
+            Runner_blue.setImage(img`
+                . . . . 2 2 2 2 2 . . . 
+                . 2 2 2 2 2 2 2 2 2 . . 
+                . . . d f d d e e e . . 
+                . d d d f d d d e d e . 
+                d d d f d d d e e d e . 
+                . f f f f d d d d e e . 
+                . . d d d d d d d . . . 
+                . . . . 2 2 2 8 2 2 . . 
+                . 2 2 2 8 2 2 8 2 2 2 . 
+                2 2 2 2 8 8 8 8 2 2 2 2 
+                d d 2 8 5 8 8 5 8 2 d d 
+                d d d 8 8 8 8 8 8 d d d 
+                d d 8 8 8 8 8 8 8 8 d d 
+                . . 8 8 8 . . 8 8 8 . . 
+                . e e e . . . . e e e . 
+                e e e e . . . . e e e e 
                 `)
         }
     }
